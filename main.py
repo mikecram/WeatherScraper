ab0bc26bb32164ece6a0ee11e32b0a90185f418d
mikecram <mikeandcram@gmail.com>
2021-05-23 12:58:17 -0400

    added multiple city functionality

--- main.py
+++ main.py
@@ -14,91 +14,103 @@ Started at: {beginTimeFormatted}''')
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+# New Hampshire Top 5 Cities
+citiesTop5NH = ["Manchester", "Nashua", "Concord", "Derry", "Dover"]
+linkListNH = {
+    "Manchester": "https://weather.com/weather/hourbyhour/l/a28b7610302e8eb27bef2d081530cbbe826326e94fa0216223d07246138cc364",
+    "Nashua": "https://weather.com/weather/hourbyhour/l/5cfead018a9910a365636982fb15a23b509c2f3bf8e8730361cf76026eaf0341",
+    "Concord": "https://weather.com/weather/hourbyhour/l/3b7ae74f5b0cd50daf16c234e487aa76668973a7f3bd06b5b5333d08ed378e39",
+    "Derry": "https://weather.com/weather/hourbyhour/l/6bbf78aeb89e180d05b11e1da942cec98b3c192065e2100743274684a33649ed",
+    "Dover": "https://weather.com/weather/hourbyhour/l/99a42d3e0de60295eaff862b481349e6967fbcbad720f5817dfb1ff1203f914b"
+}
+
 # This is the link type you need, an hour-by-hour for a city. Example link:
 # driver.get("https://weather.com/weather/hourbyhour/l/ebf31a11cc5d14b2172aa73e8a7f7689733573afc580d8a4920edcf227b95a13")
-print("Opening browser...")
-driver.get("https://weather.com/weather/hourbyhour/l/a28b7610302e8eb27bef2d081530cbbe826326e94fa0216223d07246138cc364")
+for city in citiesTop5NH:
+    print("Opening browser...")
+    driver.get(f"{linkListNH[city]}")
 
-# Give 5 seconds to load the page and all elements needed.
-# If it can't find elements correctly, try increasing the range.
-for x in range(5):
-    print(f'Page loading... {x+1}')
+    # Give 5 seconds to load the page and all elements needed.
+    # If it can't find elements correctly, try increasing the range.
+    for x in range(5):
+        print(f'Page loading... {x+1}')
+        time.sleep(1)
+
+    # Makes sure elements to be scraped are found before scraping
+    print("Locating elements...")
+    hourlyList = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--DisclosureList--OznTI"))
+        )
+    print("1 of 4 done")
+
+    locationOfScrape = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "LocationPageTitle--PresentationName--Injxu"))
+    ).text
+    # This is for use when naming the csv file
+    locationCity = locationOfScrape[0:-4]
+    locationState = locationOfScrape[-2:]
+    print("2 of 4 done")
+
+    timeOfScrape = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--timestamp--2Q9Cb"))
+    ).text
+    print("3 of 4 done")
+
+    dayOfScrape = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--longDate--3khKr"))
+    ).text
+    print("4 of 4 done")
+
+    # Define lists in order to hold 25 points of hourly data
+    hourlyDetails = []
+    detailsTime = []
+    detailsTemp = []
+    detailsPrecip = []
+
+    print("Elements located successfully.")
+    print("Scraping...")
     time.sleep(1)
 
-# Makes sure elements to be scraped are found before scraping
-print("Locating elements...")
-hourlyList = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--DisclosureList--OznTI"))
-    )
-print("1 of 4 done")
+    print(f'''**********
+    Location: {locationOfScrape}
+    Time: {timeOfScrape}
+    Day: {dayOfScrape}
+    **********''')
 
-locationOfScrape = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "LocationPageTitle--PresentationName--Injxu"))
-).text
-# This is for use when naming the csv file
-locationCity = locationOfScrape[0:-4]
-locationState = locationOfScrape[-2:]
-print("2 of 4 done")
+    # Create a csv and write the title row in a tuple
+    f = open(f"{locationCity}_{locationState}.csv", "a", newline="")
+    tupTitle = ("Timestamp", "Website Time", "City", "State", "Time","Temperature", "Precipitation")
+    writer = csv.writer(f)
+    # Comment out "writer.writerow(tupTitle)" to append to an already existing file without adding the title line.
+    writer.writerow(tupTitle)
 
-timeOfScrape = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--timestamp--2Q9Cb"))
-).text
-print("3 of 4 done")
+    # Scrape data for each hour up to 24 hours in the future
+    # then store data in lists
+    # then write rows of list data into csv
+    for i in range(25):
+        hourlyDetails.append(hourlyList.find_element_by_id(f'detailIndex{i}'))
+        detailsTime.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--daypartName--1Mebr").text)
+        detailsTemp.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--temperature--3FMlw").text)
+        detailsPrecip.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--precip--2ARnx").text)
 
-dayOfScrape = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "HourlyForecast--longDate--3khKr"))
-).text
-print("4 of 4 done")
+        # print(f'''Hours forward: {i}
+        # Time: {detailsTime[i]}
+        # Temp F°: {detailsTemp[i]}
+        # Precip: {detailsPrecip[i]}''')
 
-# Define lists in order to hold 25 points of hourly data
-hourlyDetails = []
-detailsTime = []
-detailsTemp = []
-detailsPrecip = []
+        time.sleep(0.1)
+    # Create a row to be written into the csv file
+        tup = (f"{beginTimeFormatted}", f"{timeOfScrape}", f"{locationCity}", f"{locationState}", f"{detailsTime[i]}",f"{detailsTemp[i]}", f"{detailsPrecip[i]}")
+        writer.writerow(tup)
+        print(f'''Row {i+1}/25 written to file''')
 
-print("Elements located successfully.")
-print("Scraping...")
-time.sleep(1)
+    # Close file
+    print("Closing file...")
+    f.close()
+    print(f'{city}, {locationState} weather data written to {city}_{locationState}.csv.')
 
-print(f'''**********
-Location: {locationOfScrape}
-Time: {timeOfScrape}
-Day: {dayOfScrape}
-**********''')
-
-# Create a csv and write the title row in a tuple
-f = open(f"{locationCity}_{locationState}.csv", "a", newline="")
-tupTitle = ("Timestamp", "Website Time", "City", "State", "Time","Temperature", "Precipitation")
-writer = csv.writer(f)
-# Comment out "writer.writerow(tupTitle)" to append to an already existing file without adding the title line.
-writer.writerow(tupTitle)
-
-# Scrape data for each hour up to 24 hours in the future
-# then store data in lists
-# then write rows of list data into csv
-for i in range(25):
-    hourlyDetails.append(hourlyList.find_element_by_id(f'detailIndex{i}'))
-    detailsTime.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--daypartName--1Mebr").text)
-    detailsTemp.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--temperature--3FMlw").text)
-    detailsPrecip.append(hourlyDetails[i].find_element_by_class_name("DetailsSummary--precip--2ARnx").text)
-
-    print(f'''Hours forward: {i}
-    Time: {detailsTime[i]}
-    Temp F°: {detailsTemp[i]}
-    Precip: {detailsPrecip[i]}''')
-
-    time.sleep(0.2)
-# Create a row to be written into the csv file
-    tup = (f"{beginTimeFormatted}", f"{timeOfScrape}", f"{locationCity}", f"{locationState}", f"{detailsTime[i]}",f"{detailsTemp[i]}", f"{detailsPrecip[i]}")
-    writer.writerow(tup)
-    print(f'''Row {i+1} written to file
-    ____________________''')
-
-# Close file, close web driver
-print("Closing browser...")
-f.close()
-driver.quit()
-print("Browser closed.")
+# Close browser
+driver.close()
 endTime = time.time() - beginTime
 print(f'Scrape completed in: {endTime} secs')
 
